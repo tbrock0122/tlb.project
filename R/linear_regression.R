f10b7d7a66575b8f112d35d89a3dd9713d22e0e3
Tyler Brock <tyler.brock@selu.edu>
2020-11-23 15:45:06 -0600

    additions to tutorial, edits to functions

--- R/linear_regression.R
+++ R/linear_regression.R
@@ -1,7 +1,7 @@
 #' Run a linear regression analysis of two chosen variables in a dataset, then print summary to screen.
 #' 
 #' @param variable_1: response variable (y-axis/dependent)
-#' @param varible_2: predictor variable (x-axis/independent)
+#' @param varible_2: numeric predictor variable (x-axis/independent)
 #' @return table of summary statistics printed to screen
 #' @importFrom assertthat assert_that
 #' @importFrom stats lm
@@ -9,7 +9,6 @@
 
 
 linear_regression <- function(variable_1, variable_2) {
-  assertthat::is.scalar(variable_1)
   assertthat::is.scalar(variable_2)
   model_fit <- lm(variable_1 ~ variable_2)
   summary <- summary(model_fit)
--- R/linear_regression_full.R
+++ R/linear_regression_full.R
@@ -1,7 +1,7 @@
 #' Run a linear regression analysis of two chosen variables in a dataset, check normality, then print resulting graph to screen.
 #' 
 #' @param variable_1: response variable (y-axis/dependent)
-#' @param varible_2: predictor variable (x-axis/independent)
+#' @param varible_2: numeric predictor variable (x-axis/independent)
 #' @return qqline graph printed to screen
 #' @importFrom assertthat assert_that
 #' @importFrom stats lm
@@ -11,7 +11,6 @@
 
 
 linear_regression_full <- function(variable_1, variable_2) {
-  assertthat::is.scalar(variable_1)
   assertthat::is.scalar(variable_2)
   model_fit <- lm(variable_1 ~ variable_2)
   summary <- summary(model_fit)
