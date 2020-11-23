698a86648e468a027c39a0b8a3cacc4951ab8379
Tyler Brock <tyler.brock@selu.edu>
2020-11-23 11:58:02 -0600

    added anova function

---
+++ R/simple_anova.R
@@ -0,0 +1,16 @@
+#' Run an ANOVA for chosen variables in a dataset, then print summary to screen.
+#' 
+#' @param variable_1: response variable (y-axis/dependent)
+#' @param varible_2: categorical predictor variable (x-axis/independent)
+#' @return table of summary statistics printed to screen
+#' 
+#' @export
+
+
+simple_anova <- function(variable_1, variable_2) {
+  assertthat::is.scalar(variable_1)
+  model_fit <- lm(variable_1 ~ variable_2)
+  anova_model_fit <- aov(model_fit)
+  summary <- summary(anova_model_fit)
+  return(summary)
+}
